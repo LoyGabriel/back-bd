aab7f1ff0e73f1c7dc0cd89cad519d2324fdb5e7
Nicolas <nicolaspucci1989@gmail.com>
2020-11-13 20:48:01 -0300

    cambios menores

--- routes/canciones.js
+++ routes/canciones.js
@@ -1,50 +1,45 @@
-const express = require('express')
-const router = express.Router()
-const Cancion = require('../models/Cancion')
+const express = require("express");
+const router = express.Router();
+const Cancion = require("../models/Cancion");
 
 // Buscar todas las canciones activas
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const canciones = await Cancion.find(
-      {isActive: true}
-    )
-    res.json(canciones)
+    const canciones = await Cancion.find({ isActive: true });
+    res.json(canciones);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
   }
-})
+});
 
 // Buscar todas las canciones activas, contar la cantidad de descargas y comentarios
-router.get('/list', async (req, res) => {
+router.get("/list", async (req, res) => {
   try {
     const canciones = await Cancion.aggregate([
-        {
-          $match:
-            {
-              isActive: true
-            }
+      {
+        $match: {
+          isActive: true,
         },
-        {
-          $project:
-            {
-              titulo: "$titulo",
-              fechaDePublicacion: "$fechaDePublicacion",
-              categoria: "$categoria",
-              extension: "$extension",
-              descargas: {$size: "$descargas"},
-              comentarios: {$size: "$comentarios"}
-            }
-        }
-      ]
-    )
-    res.json(canciones)
+      },
+      {
+        $project: {
+          titulo: "$titulo",
+          fechaDePublicacion: "$fechaDePublicacion",
+          categoria: "$categoria",
+          extension: "$extension",
+          descargas: { $size: "$descargas" },
+          comentarios: { $size: "$comentarios" },
+        },
+      },
+    ]);
+    res.json(canciones);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
   }
-})
+});
 
 // Agregar cancion
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const cancion = new Cancion({
     _id: 6, // TODO: autoincrementar id o usar el que da mongo
     isActive: true,
@@ -52,53 +47,53 @@ router.post('/', async (req, res) => {
     categoria: req.body.categoria,
     extension: req.body.extension,
     descargas: [],
-    comentarios: []
-  })
+    comentarios: [],
+  });
 
   try {
-    const cancionGuardada = await cancion.save()
-    res.json(cancionGuardada)
+    const cancionGuardada = await cancion.save();
+    res.json(cancionGuardada);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
   }
-})
+});
 
 // Buscar cancion por id
-router.get('/:cancionID', async (req, res) => {
+router.get("/:cancionID", async (req, res) => {
   try {
-    const cancion = await Cancion.findById(req.params.cancionID)
-    res.json(cancion)
+    const cancion = await Cancion.findById(req.params.cancionID);
+    res.json(cancion);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
   }
-})
+});
 
 // Eliminar cancion
 // TODO: agregar logica para eliminar archivo
-router.patch('/delete/:cancionID', async (req, res) => {
+router.patch("/delete/:cancionID", async (req, res) => {
   try {
     const cancion = await Cancion.updateOne(
-      {_id: req.params.cancionID},
-      {$set: {isActive: false}}
-    )
-    res.json(cancion)
+      { _id: req.params.cancionID },
+      { $set: { isActive: false } }
+    );
+    res.json(cancion);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
   }
-})
+});
 
 // Actualizar cancion
 // TODO: agregar mas campos que se modifican, definir que viene del front
-router.patch('/:cancionID', async (req, res) => {
+router.patch("/:cancionID", async (req, res) => {
   try {
     const cancion = await Cancion.updateOne(
-      {_id: req.params.cancionID},
-      {$set: {nombre: req.body.nombre}}
-    )
-    res.json(cancion)
+      { _id: req.params.cancionID },
+      { $set: { nombre: req.body.nombre } }
+    );
+    res.json(cancion);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
