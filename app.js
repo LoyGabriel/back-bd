80b1f274f99794b5a97801e327b9d6f24bc5c61a
Nicolas Pucci <nicolaspucci1989@gmail.com>
2020-11-15 19:08:23 -0300

    autorizando patch

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cancionesRoute = require('./routes/canciones')
-const atlasSchesma= 'mongodb+srv://user:mongoDB1234!@cluster0.tj8kp.mongodb.net/mongo?retryWrites=true&w=majority'
-const localSchema='mongodb://localhost:27017'
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cancionesRoute = require("./routes/canciones");
+const atlasSchesma =
+  "mongodb+srv://user:mongoDB1234!@cluster0.tj8kp.mongodb.net/mongo?retryWrites=true&w=majority";
+const localSchema = "mongodb://localhost:27017";
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, DELETE, PATCH"
+  );
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
   next();
-})
-app.use(bodyParser.json())
-app.use('/cancion', cancionesRoute)
+});
+app.use(bodyParser.json());
+app.use("/cancion", cancionesRoute);
 
-app.get('/', (req, res) => {
-  res.send('Home')
-})
+app.get("/", (req, res) => {
+  res.send("Home");
+});
 
 mongoose.connect(
   // atlasSchesma
-  localSchema
-  ,
+  localSchema,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   },
-  () => console.log('conectado a db'))
+  () => console.log("conectado a db")
+);
 
-app.listen(3000)
+app.listen(3000);
